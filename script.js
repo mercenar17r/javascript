021c2d7d1b4a9ca15abce5438a74966a0eee4e29
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-05-11 23:48:46 +0300

    add task el

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const form = document.querySelector("#new-task-form");
 const input = document.querySelector("#new-task-input");
 const list_el = document.querySelector("#tasks");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const task = input.value;
+
+  const task_el = document.createElement("div");
+  task_el.classList.add("task");
+
+  const task_content_el = document.createElement("div");
+  task_content_el.classList.add("content");
+
+  task_el.appendChild(task_content_el);
+});
